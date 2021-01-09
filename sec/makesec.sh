5c5c433717b9efbae802fb23893d20688bfda8a8
nu11secur1ty <venvaropt@gmail.com>
2021-01-09 10:06:15 +0200

    Update makesec.sh

--- sec/makesec.sh
+++ sec/makesec.sh
@@ -3,6 +3,8 @@ echo "Give username"
 read user
 echo "Give password"
 read pass
+echo "Give storesec_file"
+read store
 # Protect 
-htpasswd -b -c protect $user $pass
+htpasswd -b -c $store $user $pass
   exit 0;
